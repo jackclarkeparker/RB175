ed7bf4774266c83aa9c08f93fd2f135427b7db69
Jack <jclarkeparker@gmail.com>
2021-11-09 15:07:29 +1300

    after looking at the hint, and then Launch School's solution

--- lesson_3/book_viewer/book_viewer.rb
+++ lesson_3/book_viewer/book_viewer.rb
@@ -7,8 +7,11 @@ require "tilt/erubis"
 
 
 
-get "/" do
+get "/" do  
+  # My first try
+
   @files = []
+
   Dir.foreach("public") do |file|
     @files << file if File.file?("public/#{file}")
   end
@@ -16,6 +19,19 @@ get "/" do
   @files.sort! do |a, b|
     params["order"] == "descending" ? b <=> a : a <=> b
   end
+  
+  # After reading hint about glob
+
+  @files = Dir.glob("*", base: "public").select do |filename|
+    File.file?("public/#{filename}")
+  end.sort! do |a, b|
+    params["order"] == "descending" ? b <=> a : a <=> b
+  end
+
+  # LS solution
+
+  @files = Dir.glob("public/*").map {|file| File.basename(file) }.sort
+  @files.reverse! if params[:sort] == "desc"
   
   erb :index
 
