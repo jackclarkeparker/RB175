065ece6fb698dfff6fe585f1cfe331e020268760
Jack <jclarkeparker@gmail.com>
2021-11-18 11:11:37 +1300

    refactored search solution somewhat, more similar now to LS solution

--- lesson_3/book_viewer/book_viewer.rb
+++ lesson_3/book_viewer/book_viewer.rb
@@ -5,26 +5,140 @@ require "sinatra/reloader"
 # constantly restart the server to verify changes.
 require "tilt/erubis"
 
+require 'pry'
+require 'pry-byebug'
+
 helpers do
   def in_paragraphs(text)
-    result = ""
-    text.split("\n\n").each do |para|
-      result << "<p>#{para}</p>"
+    result = ''
+    text.split("\n\n").each_with_index do |para, index|
+      result << "<p id='#{index + 1}'>#{para}</p>"
     end
+    binding.pry
     result
   end
 
-  def search_matches
-    matches = (1..@contents.size).to_a
-    matches.select do |num|
-      chapter_text = File.read("data/chp#{num}.txt")
-      chapter_text.downcase.include?(params[:query].downcase)
+  # def highlight_query_matches(para)
+  #   para
+  # end
+end
+
+def each_chapter
+  @contents.each_with_index do |name, idx|
+    number = idx + 1
+    text = File.read("data/chp#{number}.txt")
+    yield name, number, text
+  end
+end
+
+def chapters_matching(query)
+  results = []
+
+  return results if !query || query.empty?
+
+  each_chapter do |name, number, text|
+    if text.include? query
+      matching_paragraphs = find_matching_paragraphs(query, text)
+      results << {
+        name: name,
+        number: number,
+        matching_paragraphs: matching_paragraphs
+      }
     end
   end
+
+  results
+end
+
+def find_matching_paragraphs(query, text)
+  matches = []
+
+  text.split("\n\n").each_with_index do |paragraph, idx|
+    matches << [paragraph, idx + 1] if paragraph.include?(query)
+  end
+
+  matches
+
+  # matches = text.split("/n/n").select do |para|
+  #   para.include?(query)
+  # end
+
+  # matches.map do |para|
+  #   prefix_length = "<p id='".length
+  #   id = para[prefix_length...para.index("'", prefix_length)]
+  #   [para, id]
+  # end
 end
 
 =begin
 
+PEDAC for finding_matching_paragraphs
+
+The new feature that we want is to be able to see which paragraphs of a
+matching chapter include the query.
+- We want to turn these paragraphs into anchor elements themselves, that will
+  automatically scroll to the correct portion of the page to display them.
+  - We can do this by giving `id` attributes to the paragraphs.
+
+- Does this mean that we need to arrange our text by paragraphs? Like an array
+  of paragraphs?
+  - Should we therefore backtrack on how we're currently displaying text?
+    - No, we're already capturing text in paragraphs. The solution we had
+      earlier didn't recognize paragraphs as being paragraphs (when we were
+      inserting double line breaks whenever the current line from ::readlines
+      was a string consisting of a single newline).
+      This would make it harder to provide `id` attributes for actual entire
+      paragraphs)
+    - Should we provide an `id` that is based on the index of each paragraph?
+      - Are there any problems with providing id's that are identical?
+        I thought I read somewhere that an id should be unique to an element...
+        But maybe there's no issue, because the id's might be the same between
+        different chapters, but each chapter will have a different URL?
+      - There IS no issue, use the index to provide the id.
+
+- So if we look at the output of `in_paragraphs`, it returns a string
+  - We want to be able to select portions of the output
+    - Portions whose size is determined by their paragraphing
+      - It makes sense to capture paragraphs as separate elements, that is the
+        level of discreteness that we want to target.
+
+- Input - String
+- Output - String
+* id component of the string
+
+- Each string will begin with an opening <p> tag
+  - This tag will contain an id attribute
+    - The combination of `<p` and `id='` attribute will contain a fixed number
+      of characters.
+      - If we slice from this index, up till the next quotation mark, then we
+        will collect exactly and only the id.
+
+PEDAC for highlight_query_matches
+
+Input - String
+* HTML <p> element
+Output - String
+* The same value as Input, but with `<strong>` tags enclosing all query matches
+
+- Where a query contains more than one word, a match requires that both words
+  be present in the same order.
+- If the query in it's entirety is present, along with extra characters, this
+  still counts as a match.
+  - The bold tags must only apply to the portion of the string that matches the
+    query; the extra characters must be ignored.
+- Where the query is present more than once, shall we apply bold tags to all
+  matches? Yes
+
+GENERAL APPROACH
+
+- So I suppose we can find the index of the location of a match, combine this
+  with the length of the query, and insert <strong> tags before and after these?
+  - How do we ensure we find the index of all matches?
+    - If we scan for matches, we will have an array of all the matches
+
+
+PEDAC for initial search method; for finding and listing chapters that match a
+query
 PEDAC
 
 QUESTIONS
@@ -110,6 +224,13 @@ Learning is this: If you're going to be referencing a number of data-points
 relating to some sort of object, it may be useful to capture them as name-value
 pairs so that their presence is self-explanatory when you reference them.
 
+ADDITIONALLY
+By having a method that dedicates itself to yielding information about chapters,
+it's easy to modify in one place when we want to expand the range of data we are
+collecting. Now we want to capture paragraphs that match, we can make this
+another datapoint for the same method.
+
+
 =end
 
 before do
@@ -123,16 +244,19 @@ get "/" do
 end
 
 get "/chapters/:number" do
-  num = params[:number].to_i
-  @title = @contents[num - 1]
-  @text = File.read("data/chp#{num}.txt")
+  number = params[:number].to_i
+  @title = @contents[number - 1]
+  @text = File.read("data/chp#{number}.txt")
 
-  redirect "/" unless (1..@contents.size).cover?(num)
+  redirect "/" unless (1..@contents.size).cover?(number)
 
   erb :chapter
 end
 
 get "/search" do
+  @title = "Search"
+  @results = chapters_matching(params[:query])
+
   erb :search
 end
 
